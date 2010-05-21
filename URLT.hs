741d25f983c73fa96ba3be1dabf756624e372a9a
Sergey Mironov <ierton@gmail.com>
2010-05-22 03:09:50 +0400

    urlt: Works in general!

--- URLT.hs
+++ URLT.hs
@@ -1,35 +1,23 @@
 {-# LANGUAGE TemplateHaskell, TypeFamilies, TypeSynonymInstances, EmptyDataDecls #-}
 {-# OPTIONS_GHC -F -pgmF trhsx #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances, TypeFamilies, FlexibleContexts#-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
-import Control.Applicative(Applicative((<*>),pure), (<$>), (*>))
-import Control.Applicative.Error
-import Control.Monad.Consumer(next)
-import qualified Data.ByteString.Char8 as S
-import qualified Data.ByteString.Lazy.Char8 as L
-import Data.Time (UTCTime, getCurrentTime)
-import Generics.Regular
-import URLT.Base
 import URLT.PathInfo
 import URLT.Happstack
 import URLT.TH
 import URLT.HandleT
 import URLT.XMLGenT
 import URLT.Monad
-import URLT.Regular
-import Text.XHtml.Transitional
-import System.FilePath((</>))
+import Control.Monad.Trans
 
 import Happstack.Server
 import HSP 
-import Data.Char
 import HSP.ServerPartT
+import HSX.XMLGenerator hiding(XML)
 
--- NOTE: in these examples the homepage is:
--- http://localhost:3000/MyHome
-
--- The URL / route types
 
 data BlogURL
   = BlogHome
@@ -56,43 +44,6 @@ handlerMap :: ServerPartT IO Response
 handlerMap = do
     implSite "" "" site
 
--- the SiteURL route handler Application
--- In this version, the function for converting the URL type to its
--- String representation is passed in as the argument mkAbs.
-page :: XMLGenT (URLT SiteURL (ServerPartT IO)) XML
-page =
-    <html>
-     <head>
-      <title>Hello</title>
-     </head>
-     <body>
-      <p>Hello</p>
-     </body>
-    </html>
-
-mySite :: SiteURL -> URLT SiteURL (ServerPartT IO) Response
-mySite url = do
-    xml <- unXMLGenT page
-    makeResponse xml
-
-makeResponse = ok . setHeader "Content-Type" "text/html" . toResponse . renderAsHTML
-
---     main <- showURL $ MyHome
---     ok $ setHtml $ toResponse $ renderHtml $ toHtmlFromList [
---             header `hwith` [
---                 meta ! [ 
---                     strAttr "http-equiv" "Content-Type", 
---                     strAttr "content" "text/html", 
---                     strAttr "http-equiv" "text/html; charset=UTF-8" 
---                     ],
---                 thetitle << "MyHome"
---             ] ,
---             body `hwith` [
---                 anchor ! [ href $ main ] << "Home sweet home"
---             ]
---         ]
-
-
 
 site :: Site SiteURL String (ServerPartT IO Response)
 site = Site {
@@ -101,5 +52,59 @@ site = Site {
     formatLink = toPathInfo , 
     parseLink = fromPathInfo 
     }
+
+mySite :: SiteURL -> URLT SiteURL (ServerPartT IO) Response
+mySite MyHome = unXMLGenT (about >>= page) >>= makeResponse
+mySite (MyBlog BlogHome) = unXMLGenT (blog "cats" >>= page) >>= makeResponse
+mySite (MyBlog (BlogPost x)) = unXMLGenT (blog x >>= page) >>= makeResponse
+
+makeResponse = ok . setHeader "Content-Type" "text/html" . toResponse . renderAsHTML
+
+instance (EmbedAsAttr m (Attr String c), TypeCastM m1 m) => EmbedAsAttr m (Attr String (XMLGenT m1 c)) where
+    asAttr (n := (XMLGenT m1a)) = do
+            a <- XMLGenT $ typeCastM m1a
+            asAttr (n:=a)
+
+page :: XML -> XMLGenT (URLT SiteURL (ServerPartT IO)) XML
+page content =
+    <html>
+     <head>
+      <title>Holy Homepage</title>
+     </head>
+     <body>
+      <p>Hello there</p>
+      <% content %>
+      <% qqq %>
+       <a href=(qqq)>Blog</a>
+     </body>
+    </html>
+
+qqq :: XMLGenT (URLT SiteURL (ServerPartT IO)) String
+qqq = do
+    x <- showURL (MyBlog $ BlogPost "hello-world")
+    return x
+
+-- FIXME: Why or why this doesn't work???
+-- page' :: XMLGenT (ServerPartT IO) XML
+-- page' = 
+--     <html>
+--      <head>
+--       <title>Holy Homepage</title>
+--      </head>
+--      <body>
+--       <p>Hello there</p>
+--       <% nn %>
+      -- <a href=(n)>Blog</a>
+--      </body>
+--     </html>
+
+-- nn :: ServerPartT IO XML
+-- nn = undefined
+
+about :: XMLGenT (URLT SiteURL (ServerPartT IO)) XML
+about = <div>When you worry call me, i make you happy.</div>
+
+blog :: String -> XMLGenT (URLT SiteURL (ServerPartT IO)) XML
+blog what = <div>This is kinda blog post about <% what %></div>
 
 
