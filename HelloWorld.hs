846139f868606d53dba8fbb1157bb1b8e20acf57
Sergey Mironov <ierton@gmail.com>
2010-06-04 15:34:07 +0400

    hw: Remove *lhs stuff

---
+++ HelloWorld.hs
@@ -0,0 +1,34 @@
+-- Author: Sergey Mironov
+-- See also http://www.ierton.ru
+
+{-# OPTIONS_GHC -F -pgmF trhsx #-}
+
+import Happstack.Server
+import HSP 
+import Data.Char
+import HSP.ServerPartT
+
+main :: IO ()
+main = do
+  let cfg = Conf { validator = Nothing, port = 3333 }
+  simpleHTTP cfg handlerMap
+
+handlerMap :: ServerPartT IO Response
+handlerMap = do
+  xhello <- unXMLGenT (hello "World")
+  makeResponse xhello
+
+makeResponse = ok . setHeader "Content-Type" "text/html" . 
+  toResponse . renderAsHTML
+
+
+hello :: String -> XMLGenT (ServerPartT IO) XML
+hello noun =
+    <html>
+     <head>
+      <title>Hello, <% noun %></title>
+     </head>
+     <body>
+      <p>Hello, <% map toUpper noun %></p>
+     </body>
+    </html>
