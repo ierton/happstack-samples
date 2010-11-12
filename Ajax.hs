a37809773701a12e2c2d798631ff64793723a81d
Sergey Mironov <ierton@gmail.com>
2010-11-12 13:55:28 +0300

    Ajax.hs: cleanups

--- Ajax.hs
+++ Ajax.hs
@@ -11,6 +11,9 @@ import Control.Monad.Reader
 import Control.Monad.Maybe
 import System.Log.Logger
 
+-- Link to JQuery source
+jQuery = "http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"
+
 main :: IO ()
 main = do
     updateGlobalLogger rootLoggerName (setLevel DEBUG)
@@ -25,43 +28,29 @@ handlerMap = msum [
 renderHello :: ServerPartT IO Response
 renderHello = do
     xhello <- unXMLGenT hello
-    makeHtmlResponse xhello
+    makeResponse . renderAsHTML $ xhello
 
 renderAjax :: ServerPartT IO Response
-renderAjax = makeAjaxResponse "bla-bla-bal"
+renderAjax = do
+    makeResponse $ "bla-bla-bal"
 
-makeHtmlResponse = ok . setHeader "Content-Type" "text/html" .  toResponse . renderAsHTML
-makeAjaxResponse = ok . setHeader "Content-Type" "text/html" .  toResponse
-
-errMsg :: String
-errMsg = "As you can see, the link no longer tooks you to jquery.com"
-
-urlTarget = "http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"
+makeResponse = ok . setHeader "Content-Type" "text/html" .  toResponse
 
 hello :: XMLGenT (ServerPartT IO) XML
 hello =
     <html lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <script src=(urlTarget) type="text/javascript"></script>
+        <script src=(jQuery) type="text/javascript"></script>
         <script type="text/javascript">
             $(document).ready( function() {
-                $("a").click(function(event) {
-                    alert("<% errMsg %>");
-                    event.preventDefault();
-                    $("a").addClass("test");
-                });
                 $("#generate").click(function() {
-                   $("#quote").load("_ajax/tellme.php");
+                   $("#quote").load("_ajax");
                 });
             });
         </script> 
-        <style type="text/css">
-            a.test { font-weight: bold; }
-        </style>
     </head>
     <body>
-        <a href="http://jquery.com/">Click me</a>
         <input type="submit" id="generate" value="Generate!"/>
         <div id="quote"></div>
     </body>
